10b84a4b13805a551181c0533e17f6fcb5bf738f
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-07-31 21:09:39 +0430

    entities are added

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string? UserName { get; set; }
+        public List<ShoppingCartItem>? Items { get; set; }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+            
+        }
+
+        public decimal? TotalPrice =>
+            Items?
+                .Sum(i => i.Price * i.Quantity)
+            ?? 0;
+    }
+}
