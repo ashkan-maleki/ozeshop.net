d7ba9c673625cbf337e4518b05db8d498619b35b
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-08-01 17:47:20 +0430

    basket controllers and repositories are added!

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        private readonly ILogger<BasketController> _logger;
+
+
+        public BasketController(
+            IBasketRepository repository, 
+            ILogger<BasketController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),
+            (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>>
+            GetBasket(string userName)
+            => Ok(await
+                _repository.GetBasket(userName) ?? 
+                  new ShoppingCart(userName));
+
+        [HttpPost()]
+        [ProducesResponseType(typeof(ShoppingCart),
+            (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>>
+            UpdateBasket([FromBody] ShoppingCart basket)
+            => Ok(await _repository.UpdateBasket(basket));
+
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(ShoppingCart),
+            (int) HttpStatusCode.OK)]
+        public async Task<ActionResult>
+            DeleteBasket([FromBody] string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }    
+        
+
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using JsonConverter = System.Text.Json.Serialization.JsonConverter;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart?> GetBasket(string userName);
+        Task<ShoppingCart?> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+
+    class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+        public async Task<ShoppingCart?> GetBasket(string userName)
+        {
+            var basket = await 
+                _redisCache.GetStringAsync(userName);
+            if (!String.IsNullOrEmpty(basket))
+            {
+                return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+            }
+
+            return null;
+        }
+
+        public async Task<ShoppingCart?> 
+            UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName,
+                JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName ?? "");
+        }
+
+        public async Task DeleteBasket(string userName)
+            => await _redisCache.RemoveAsync(userName);
+    }
+}
