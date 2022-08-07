0e6298dc296858540bf4cebfff10829f6366045f
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-08-07 21:36:37 +0430

    discount grpc client is added and developed

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService
+            .DiscountProtoServiceClient _discountService;
+
+        public DiscountGrpcService(DiscountProtoService
+            .DiscountProtoServiceClient discountService)
+        {
+            _discountService = discountService;
+        }
+
+        public async Task<CouponModel> GetDiscountAsync(
+            string productName)
+            => await _discountService.GetDiscountAsync(new
+                GetDiscountRequest
+                {
+                    ProductName = productName
+                });
+    }
+}
