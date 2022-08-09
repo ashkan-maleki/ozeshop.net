fccbf94973378725b01c264b81b1cca83bd92e4e
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-08-09 19:31:10 +0430

    application contracts are built

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IAsyncOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserNameAsync(string userName);
+    }
+}
