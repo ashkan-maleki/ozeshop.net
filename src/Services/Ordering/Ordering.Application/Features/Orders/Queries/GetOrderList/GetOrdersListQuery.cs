c18cab40cb4258502ca77f6f27db220e46bf05ca
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-08-12 18:35:28 +0430

    Order queries are created

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public GetOrdersListQuery(string? userName)
+        {
+            UserName = userName;
+        }
+
+        public string? UserName { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQueryHandler
+        : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IAsyncOrderRepository _repo;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(
+            IAsyncOrderRepository repo,
+            IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(
+            GetOrdersListQuery request,
+            CancellationToken cancellationToken)
+            =>
+                _mapper.Map<List<OrdersVm>>(
+                    await _repo.GetOrdersByUserNameAsync(
+                        request.UserName)
+                );
+
+    }
+}
