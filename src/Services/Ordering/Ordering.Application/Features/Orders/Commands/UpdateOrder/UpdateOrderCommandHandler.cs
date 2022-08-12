cecfb6141042d9079f939fd91686fd43b674d70e
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-08-12 19:18:47 +0430

    Update order command is added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IAsyncOrderRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IAsyncOrderRepository repo,
+            IMapper mapper, 
+            ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _repo = repo;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await 
+                _repo.GetByIdAsync(request.Id.Value);
+            if (orderToUpdate == null)
+            {
+                _logger.LogError("Order does not exist on database");
+
+            }
+
+            _mapper.Map(request, orderToUpdate,
+                typeof(UpdateOrderCommand), 
+                typeof(Order));
+
+            await _repo.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated");
+            return Unit.Value;
+        }
+    }
+}
