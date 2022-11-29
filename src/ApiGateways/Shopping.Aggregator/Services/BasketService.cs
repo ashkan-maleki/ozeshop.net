7e6bdb3f19d9b6649d90433b881598b3314b23e2
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-11-29 21:45:31 +0330

    adding and configuring aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+class BasketService : IBasketService
+{
+    public Task<BasketModel> GetBasket(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+class OrderService : IOrderService
+{
+    public Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
