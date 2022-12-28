201a32921413bf1b41768c6327b11d04ee36fc78
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-28 18:16:44 +0330

    Configure MongoDb Config

---
+++ src/Services/Catalog/Catalog.Query/Catalog.Query.Infrastructure/Persistence/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Common.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.Query.Infrastructure.Persistence;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IOptions<MongoDbConfig> options)
+    {
+        MongoDbConfig config = options.Value;
+        IMongoClient client = new MongoClient(config.ConnectionString);
+        IMongoDatabase db = client.GetDatabase(config.Database);
+        CreateModel(db);
+    }
+
+    private void CreateModel(IMongoDatabase db)
+    {
+        
+    }
+}
---
+++ src/Services/Catalog/Catalog.Query/Catalog.Query.Infrastructure/Persistence/ICatalogContext.cs
@@ -0,0 +1,6 @@
+namespace Catalog.Query.Infrastructure.Persistence;
+
+public interface ICatalogContext
+{
+    // public IMongoCollection<Product> Products { get; }
+}
