8bb4916252e8c7a0de1d2568d5610189789d8cbb
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-01-23 23:39:07 +0330

    Add product view model

---
+++ src/Services/Catalog/Catalog.Query/Catalog.Query.Application/DTOs/ProductVm.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Query.Domain.Entities;
+
+namespace Catalog.Query.Application.DTOs;
+
+public record ProductVm(
+    string? Id,
+    string? Name,
+    string? Category,
+    string? Summary,
+    string? Description,
+    string? ImageFile,
+    Decimal? Price
+);
