46bf4de4c764f535a2dc540c4b13378ec89d39b1
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-01-23 23:39:38 +0330

    Add product list handler

---
+++ src/Services/Catalog/Catalog.Query/Catalog.Query.Application/Handlers/GetProductListQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Query.Application.DTOs;
+using Catalog.Query.Application.Queries;
+using MediatR;
+
+namespace Catalog.Query.Application.Handlers;
+
+public class GetProductListQueryHandler : IRequestHandler<GetProductListQuery, IReadOnlyList<ProductVm>>
+{
+    public Task<IReadOnlyList<ProductVm>> Handle(GetProductListQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
